2a59489ff6e22a3e38a5d7e9483193591bd32141
srameo <870830732@qq.com>
2021-04-22 22:12:23 +0800

    解除randomGetter的请求上限

--- cloudbase/cloud1-7g5rittif1dd74f6/functions/randomGetter/index.js
+++ cloudbase/cloud1-7g5rittif1dd74f6/functions/randomGetter/index.js
@@ -1,21 +1,22 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: 'cloud1-7g5rittif1dd74f6'
+})
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
 
-  result = null
-  await db.collection('chars_with_res')
-          .aggregate()
-          .sample({
-            size: event.size
-          }).end().then((res) => result = res)
-
   try {
-    return result
+    return db.collection("chars_with_res")
+             .aggregate()
+             .sample({
+               size: event.size
+             })
+             .limit(event.size)
+             .end()
   } catch (e) {
     console.log(e)
   }
