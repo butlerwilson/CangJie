f6c6e4997eee0c0a85f95cdd10c2b2d7ab6379f4
srameo <870830732@qq.com>
2021-04-22 21:25:46 +0800

    data filter and json formatter

---
+++ data/data_handler.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+from requests.models import guess_json_utf
+
+def filter(in_file):
+    print("reading wikipedia ...")
+    start_html = requests.get("https://zh.wiktionary.org/wiki/Appendix:通用规范汉字表")
+    soup = BeautifulSoup(start_html.text, 'lxml')
+    _as = soup.find(attrs={"class": "wikitable"})
+    chars = [a.text for a in _as.find_all(name="a")]
+    lines = []
+    print("filtering ...")
+    with open(in_file, "r") as chaizi:
+        for line in chaizi.readlines():
+            if line[0] in chars and line not in lines:
+                lines.append(line.strip())
+    return lines
+
+def json_formatter(lines):
+    json_list = []
+    for idx, line in enumerate(lines):
+        kv = line.split("\t")
+        key = kv[0]
+        values = kv[1::]
+        json_list.append({
+            "_id": idx,
+            "key": key,
+            "value": [v.split(" ") for v in values]
+        })
+    return json_list
+
+
+if __name__ == "__main__":
+    with open("./temp.json", "w") as jf:
+        lines = filter("chaizi-jt.txt")
+        js = json_formatter(lines)
+        for ljs in js:
+            jf.write(json.dumps(ljs))
