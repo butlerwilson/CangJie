b3ba0f675f645c17020fdbea1d45aa3409163a1d
srameo <870830732@qq.com>
2021-04-27 15:25:53 +0800

    charList 云函数重构

--- cloudbase/cloud1-7g5rittif1dd74f6/functions/charList/index.js
+++ cloudbase/cloud1-7g5rittif1dd74f6/functions/charList/index.js
@@ -4,6 +4,25 @@ const cloud = require('wx-server-sdk')
 cloud.init({
   env: 'cloud1-7g5rittif1dd74f6'
 })
+
+function charListAppend(ans, char_list, char) {
+  ans.push(char_list.length)
+  char_list.push({
+    "char": char,
+    "ans_index": -1
+  })
+}
+
+function ansListAppend(ans_list, char_list, ans, char) {
+  for (let i = 0; i < ans.length; ++i) {
+    char_list[ans[i]]["ans_index"] = ans_list.length
+  }
+  ans_list.push({
+    "ans": ans,
+    "char": char
+  })
+}
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
@@ -28,21 +47,12 @@ exports.main = async (event, context) => {
     // 随机从两个部首列表中随机挑选加入呈现的列表中
     while (ptr1 < char_list1.length && ptr2 < char_list2.length) {
       var p = Math.random()
-      if (p > 0.5) {
-        ans1.push(char_list.length)
-        char_list.push(char_list1[ptr1++])
-      }
-      else {
-        ans2.push(char_list.length)
-        char_list.push(char_list2[ptr2++])
-      }
+      if (p > 0.5) charListAppend(ans1, char_list, char_list1[ptr1++])
+      else charListAppend(ans2, char_list, char_list2[ptr2++])
     }
     if (ptr1 == char_list1.length) {
       // 如果是第一个先全部填入呈现的列表中，则将第二个字设置为第一个
-      ans_list.push({
-        "ans": ans1,
-        "char": chars[idx].key
-      })
+      ansListAppend(ans_list, char_list, ans1, chars[idx].key)
       ans1 = ans2
       ans2 = []
       ptr1 = ptr2
@@ -50,18 +60,9 @@ exports.main = async (event, context) => {
       idx++
     } else {
       // 如果是第二个先，则直接将第一个字剩下的所有部首填入呈现的列表
-      while (ptr1 < char_list1.length) {
-        ans1.push(char_list.length)
-        char_list.push(char_list1[ptr1++])
-      }
-      ans_list = ans_list.concat([{
-        "ans": ans2,
-        "char": chars[idx + 1].key
-      },
-      {
-        "ans": ans1,
-        "char": chars[idx].key
-      }])
+      while (ptr1 < char_list1.length) charListAppend(ans1, char_list, char_list1[ptr1++])
+      ansListAppend(ans_list, char_list, ans2, chars[idx + 1].key)
+      ansListAppend(ans_list, char_list, ans1, chars[idx].key)
       ans1 = [], ans2 = []
       ptr1 = 0, ptr2 = 0
       idx += 2
@@ -72,14 +73,8 @@ exports.main = async (event, context) => {
     var ptr = 0, last_ans = []
     var index = Math.floor((Math.random()*chars[idx].value.length));
     var last_char_list = chars[idx].value[index]
-    while (ptr < last_char_list.length) {
-      last_ans.push(char_list.length)
-      char_list.push(last_char_list[ptr++])
-    }
-    ans_list.push({
-      "ans": last_ans,
-      "char": chars[idx].key
-    })
+    while (ptr < last_char_list.length) charListAppend(last_ans, char_list, last_char_list[ptr++])
+    ansListAppend(ans_list, char_list, last_ans, chars[idx].key)
   }
 
   return {
