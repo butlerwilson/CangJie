2a290ae598104c6b60ad2d249bd99b21ad56bdfa
srameo <870830732@qq.com>
2021-05-11 17:51:05 +0800

    在数据库中可以添加用户条目 若用户在登录时若没有条目，则会增加条目

--- cloudbase/cloud1-7g5rittif1dd74f6/functions/getDecryptedData/index.js
+++ cloudbase/cloud1-7g5rittif1dd74f6/functions/getDecryptedData/index.js
@@ -1,11 +1,29 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
-
+cloud.init({
+  env: 'cloud1-7g5rittif1dd74f6'
+})
+const db = cloud.database()
+const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+
+  // 如果用户从未登陆，则创建新 entry
+  var res = await db.collection("user_info")
+                    .where({
+                      _id: _.eq(wxContext.OPENID)
+                    })
+                    .get()
+  if (!res.length) {
+    db.collection("user_info").add({
+      data: {
+        _id: wxContext.OPENID,
+        chars: []
+      }
+    })
+  }
 
   return {
     event,
