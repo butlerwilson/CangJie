0cb24cba4b450b99132aaf60624049d039ff815b
srameo <870830732@qq.com>
2021-05-11 17:26:36 +0800

    新增解密用户数据的云函数

--- miniprogram/game.js
+++ miniprogram/game.js
@@ -3,4 +3,52 @@ import './js/libs/symbol'
 
 import Main from './js/main'
 
-new Main()
+// new Main()
+
+wx.login({
+  success (res) {
+    console.log(res);
+    wx.getSetting({
+      success(res) {
+        if(!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success(){
+              wx.getUserInfo({
+                success(res) {
+                  console.log(res);
+                  wx.cloud.callFunction({
+                    name: "getDecryptedData",
+                    data: {
+                      decryptedData: wx.cloud.CloudID(res.cloudID)
+                    }
+                  }).then(res => {
+                    console.log(res);
+                  }).catch(err => {
+                    console.log(err);
+                  });
+                }
+              })
+            }
+          })
+        } else {
+          wx.getUserInfo({
+            success(res) {
+              console.log(res);
+              wx.cloud.callFunction({
+                name: "getDecryptedData",
+                data: {
+                  decryptedData: wx.cloud.CloudID(res.cloudID)
+                }
+              }).then(res => {
+                console.log(res);
+              }).catch(err => {
+                console.log(err);
+              });
+            }
+          })
+        }
+      }
+    })
+  }
+})
